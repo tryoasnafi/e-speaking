f749c64ae9fdaadd50f9f9a026be9592697c42d3
muhamadRamadhaniZ <danii251018@gmail.com>
2021-06-21 12:43:39 +0700

    Add default layout template

---
+++ app/Views/layout/default.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/custom.css" />
+    <?= $this->renderSection('title') ?>
+</head>
+
+<body>
+    <!-- Top Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="<?= base_url() ?>">E-Speaking</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="<?= base_url() ?>">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#tentang">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#faq">FAQ</a>
+                    </li>
+                </ul>
+                <div class="ml-auto">
+                    <a href="<?= base_url() ?>/masuk" class="btn btn-outline-espeaking">Masuk</a>
+                    <a href="<?= base_url() ?>/daftar" class="btn btn-espeaking">Daftar</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Main Content -->
+    <div class="container-md main-content">
+        <?= $this->renderSection('content') ?>
+    </div>
+    <!-- Footer -->
+    <footer class="py-3 bg-espeaking text-center text-white">
+        <span>Copyright &copy; 2021 E-Speaking Team</span>
+    </footer>
+
+    <script src="<?= base_url() ?>/assets/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="<?= base_url() ?>/assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
