83a7250397ffa7a3d89f0973abce49cdbc4fc833
Tryo Asnafi <tryoasnafi@gmail.com>
2021-06-21 19:42:42 +0700

    Add db connection and validation to BaseController

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -54,5 +54,8 @@ class BaseController extends Controller
 		// Preload any models, libraries, etc, here.
 		//--------------------------------------------------------------------
 		// E.g.: $this->session = \Config\Services::session();
+		// $db = \Config\Database::connect();
+		$this->db = \Config\Database::connect();
+		$this->validation =  \Config\Services::validation();
 	}
 }
