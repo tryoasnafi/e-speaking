f8cdac7bd91c1978ad15c3269ff3c43239292e18
muhamadRamadhaniZ <danii251018@gmail.com>
2021-06-22 16:59:12 +0700

    Fix home template asset location

--- app/Views/home-template.php
+++ app/Views/home-template.php
@@ -30,64 +30,6 @@
                     <div class="search-element">
                         <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
                         <button class="btn" type="submit"><i class="fas fa-search"></i></button>
-                        <div class="search-backdrop"></div>
-                        <div class="search-result">
-                            <div class="search-header">
-                                Histories
-                            </div>
-                            <div class="search-item">
-                                <a href="#">How to hack NASA using CSS</a>
-                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">Kodinger.com</a>
-                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">#Stisla</a>
-                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
-                            </div>
-                            <div class="search-header">
-                                Result
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="<?= base_url() ?>/template/assets/img/products/product-3-50.png" alt="product">
-                                    oPhone S9 Limited Edition
-                                </a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="<?= base_url() ?>/template/assets/img/products/product-2-50.png" alt="product">
-                                    Drone X2 New Gen-7
-                                </a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="<?= base_url() ?>/template/assets/img/products/product-1-50.png" alt="product">
-                                    Headphone Blitz
-                                </a>
-                            </div>
-                            <div class="search-header">
-                                Projects
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <div class="search-icon bg-danger text-white mr-3">
-                                        <i class="fas fa-code"></i>
-                                    </div>
-                                    Stisla Admin Template
-                                </a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <div class="search-icon bg-primary text-white mr-3">
-                                        <i class="fas fa-laptop"></i>
-                                    </div>
-                                    Create a new Homepage Design
-                                </a>
-                            </div>
-                        </div>
                     </div>
                 </form>
                 <ul class="navbar-nav navbar-right">
@@ -291,104 +233,6 @@
                                 <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
                             </ul>
                         </li>
-                        <li class="menu-header">Stisla</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Components</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="components-article.html">Article</a></li>
-                                <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>
-                                <li><a class="nav-link" href="components-chat-box.html">Chat Box</a></li>
-                                <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty State</a></li>
-                                <li><a class="nav-link" href="components-gallery.html">Gallery</a></li>
-                                <li><a class="nav-link beep beep-sidebar" href="components-hero.html">Hero</a></li>
-                                <li><a class="nav-link" href="components-multiple-upload.html">Multiple Upload</a></li>
-                                <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a></li>
-                                <li><a class="nav-link" href="components-statistic.html">Statistic</a></li>
-                                <li><a class="nav-link" href="components-tab.html">Tab</a></li>
-                                <li><a class="nav-link" href="components-table.html">Table</a></li>
-                                <li><a class="nav-link" href="components-user.html">User</a></li>
-                                <li><a class="nav-link beep beep-sidebar" href="components-wizard.html">Wizard</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
-                                <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
-                                <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google Maps</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
-                                <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
-                                <li><a href="gmaps-geocoding.html">Geocoding</a></li>
-                                <li><a href="gmaps-geolocation.html">Geolocation</a></li>
-                                <li><a href="gmaps-marker.html">Marker</a></li>
-                                <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
-                                <li><a href="gmaps-route.html">Route</a></li>
-                                <li><a href="gmaps-simple.html">Simple</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Modules</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
-                                <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
-                                <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
-                                <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
-                                <li><a class="nav-link" href="modules-font-awesome.html">Font Awesome</a></li>
-                                <li><a class="nav-link" href="modules-ion-icons.html">Ion Icons</a></li>
-                                <li><a class="nav-link" href="modules-owl-carousel.html">Owl Carousel</a></li>
-                                <li><a class="nav-link" href="modules-sparkline.html">Sparkline</a></li>
-                                <li><a class="nav-link" href="modules-sweet-alert.html">Sweet Alert</a></li>
-                                <li><a class="nav-link" href="modules-toastr.html">Toastr</a></li>
-                                <li><a class="nav-link" href="modules-vector-map.html">Vector Map</a></li>
-                                <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
-                            </ul>
-                        </li>
-                        <li class="menu-header">Pages</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="auth-forgot-password.html">Forgot Password</a></li>
-                                <li><a href="auth-login.html">Login</a></li>
-                                <li><a class="beep beep-sidebar" href="auth-login-2.html">Login 2</a></li>
-                                <li><a href="auth-register.html">Register</a></li>
-                                <li><a href="auth-reset-password.html">Reset Password</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>Errors</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="errors-503.html">503</a></li>
-                                <li><a class="nav-link" href="errors-403.html">403</a></li>
-                                <li><a class="nav-link" href="errors-404.html">404</a></li>
-                                <li><a class="nav-link" href="errors-500.html">500</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="features-activities.html">Activities</a></li>
-                                <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
-                                <li><a class="nav-link" href="features-posts.html">Posts</a></li>
-                                <li><a class="nav-link" href="features-profile.html">Profile</a></li>
-                                <li><a class="nav-link" href="features-settings.html">Settings</a></li>
-                                <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
-                                <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="utilities-contact.html">Contact</a></li>
-                                <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                                <li><a href="utilities-subscribe.html">Subscribe</a></li>
-                            </ul>
-                        </li>
-                        <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a></li>
                     </ul>
 
                     <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
@@ -423,7 +267,7 @@
 
     <!-- General JS Scripts -->
     <script src="<?= base_url() ?>/template/node_modules/jquery/js/jquery.js"></script>
-    <script src="<?= base_url() ?>/assets/css/bootstrap.bundle.min.css"></script>
+    <script src="<?= base_url() ?>/assets/js/bootstrap.bundle.min.js"></script>
     <script src="<?= base_url() ?>/template/node_modules/jquery.nicescroll/jquery.nicescroll.min.js"></script>
     <script src="<?= base_url() ?>/template/assets/js/stisla.js"></script>
 
